faade3797a7f013f1de715df5bbb83fe4d322ff8
Haley Lenander <halenander@gmail.com>
2024-02-27 16:48:04 -0800

    resume pdf functionality

--- haley-website/src/components/NavBar.js
+++ haley-website/src/components/NavBar.js
@@ -2,26 +2,23 @@ import { useState, useEffect } from "react";
 import { Navbar, Nav, Container } from "react-bootstrap";
 import logo from '../assets/img/logo.svg';
 import navIcon1 from '../assets/img/nav-icon1.svg';
-import navIcon2 from '../assets/img/nav-icon2.svg';
-import navIcon3 from '../assets/img/nav-icon3.svg';
+import resumePDF from '../assets/pdf/resume.pdf'
 
 export const NavBar = () => {
-
     const [activeLink, setActiveLink] = useState('home');
     const [scrolled, setScrolled] = useState(false);
-  
+
     useEffect(() => {
-      const onScroll = () => {
-        if (window.scrollY > 50) {
-          setScrolled(true);
-        } else {
-          setScrolled(false);
+        const onScroll = () => {
+            if (window.scrollY > 50){
+                setScrolled(true);
+            } else {
+                setScrolled(false);
+            }
         }
-      }
-  
-      window.addEventListener("scroll", onScroll);
-  
-      return () => window.removeEventListener("scroll", onScroll);
+        window.addEventListener("scroll", onScroll);
+
+        return () => window.removeEventListener("scroll", onScroll);
     }, [])
 
     const onUpdateActiveLink = (value) => {
@@ -45,11 +42,9 @@ export const NavBar = () => {
           </Nav>
           <span className="navbar-text">
             <div className="social-icon">
-                <a href="/#"><img src={navIcon1} alt="" /></a>
-                <a href="/#"><img src={navIcon2} alt="" /></a>
-                <a href="/#"><img src={navIcon3} alt="" /></a>
+                <a href="#"><img src={navIcon1} alt = "" /></a>
             </div>
-            <button className="vvd" onClick={() => console.log('connect')}><span>Let's Connect</span></button>
+            <button className="vvd"><span><a href={resumePDF} target="_blank">Open Resume</a></span></button>
           </span>
         </Navbar.Collapse>
       </Container>
